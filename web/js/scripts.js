46d19fd7f905aad6b73b4535568265bc6ea5fa32
Imri <imri.luzzattov@gmail.com>
2020-11-12 19:42:54 +0200

    -added new logo -fixed/centered menu after logo insertion -fixed text inside "move to top" icon bottom-right

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -299,7 +299,7 @@
 
     /* Back To Top Button */
     // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    $('body').prepend('<a href="body" class="back-to-top page-scroll"></a>');
     var amountScrolled = 700;
     $(window).scroll(function() {
         if ($(window).scrollTop() > amountScrolled) {
